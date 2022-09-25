6b68c5776a8ed240911f9d78503114a11baf340d
sylph@reset.jp <NullPopPo>
2022-09-25 08:47:23 +0000

    KKS版ビルド通す

--- Cosplay Academy Sun/OutfitDecider.cs
+++ Cosplay Academy Sun/OutfitDecider.cs
@@ -6,11 +6,13 @@ namespace Cosplay_Academy
     {
         private static void Grabber(ref List<FolderData> temp2, int set, int exp)
         {
+#if false // 再検討 
             if (set == 1 && Settings.GrabSwimsuits.Value)
             {
                 temp2.AddRange(DataStruct.DefaultFolder[8].FolderData[exp].GetAllFolders());
                 return;
             }
+#endif
         }
 
         private static void SpecialProcess()
